817639a9b592ac3cb354be7256cb997a5929db95
Andre Mashraghi <andre.mashraghi@gmail.com>
2018-02-07 20:17:39 -0500

    changes

--- addjob/forms.py
+++ addjob/forms.py
@@ -1,10 +1,9 @@
 from django import forms
-from jobs.models import House
+from django.forms import ModelForm
+from jobs.models import House, Job
 
-class AddJob(forms.Form):
-    #get all houses as a queryset
-    houses = House.objects.all()
-    time = forms.DateTimeField(widget=forms.HiddenInput())
-    house = forms.ModelMultipleChoiceField(required=True, queryset=houses)
-    start_amount = forms.DecimalField(max_digits=10, decimal_places=2)
-    document = forms.FileField(label='Upload the invoice document')
+#a form that has all the attributes of the Job class from the jobs app
+class AddJob(ModelForm):
+    class Meta:
+        model = Job
+        exclude = ['company', 'total_paid']
