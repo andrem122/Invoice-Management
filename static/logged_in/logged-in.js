894ae75568a7c94eb8b36023849d4cc79198a43a
Andre Mashraghi <andre.mashraghi@gmail.com>
2018-04-09 22:04:41 -0400

    changed logged-in ui design

--- static/logged_in/logged-in.js
+++ static/logged_in/logged-in.js
@@ -29,4 +29,20 @@ $(document).ready(function(){
     }
   });
 
+  //submits a form when button is clicked
+  function post_form(class_names) {
+    document.addEventListener('click', function(e){
+      classes = Array.from(e.target.classList); //convert DOMTicketList to array
+      l = class_names.length;
+      for (var i = 0; i < l; i++) {
+        if(classes.includes(class_names[i])) {
+          var form = e.target.parentElement.parentElement;
+          form.submit();
+        }
+      }
+    });
+  }
+
+  post_form(['v-payment-history-p']);
+
 });
