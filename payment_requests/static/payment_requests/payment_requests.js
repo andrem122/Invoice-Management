a84b36f5ae6a85c142f8be0647a15d7668ba72f2
Andre Mashraghi <andre.mashraghi@gmail.com>
2018-04-09 21:39:50 -0400

    changed logged-in ui design

--- payment_requests/static/payment_requests/payment_requests.js
+++ payment_requests/static/payment_requests/payment_requests.js
@@ -4,15 +4,17 @@ document.addEventListener('DOMContentLoaded', function(event) {
   document.addEventListener('click', function(e){
     classes = Array.from(e.target.classList); //convert DOMTicketList to array
     console.log(e.target.className);
-    if(classes.includes('upload-document-btn-p')) {
+    if(classes.includes('upload-document-btn-p') || classes.includes('unapprove-payment-p')) {
       clicks += 1;
       var form = e.target.parentElement.parentElement;
       var document_upload = form.children[1];
-      if(clicks === 1) {
+      if(clicks === 1 && classes.includes('upload-document-btn-p')) {
         e.preventDefault();
         document_upload.setAttribute('type', 'file');
       } else {
-        if(document_upload.value !== '') {
+        if(document_upload.value !== '' && classes.includes('upload-document-btn-p')) {
+          form.submit();
+        } else {
           form.submit();
         }
       }
