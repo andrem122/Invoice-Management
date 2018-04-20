fcf3804587b4c2dbe546a3dcf0660b815ee57fe5
Andre Mashraghi <andre.mashraghi@gmail.com>
2018-04-19 20:09:34 -0400

    implemented customer and staff check before viewing the projects view

--- projects/views.py
+++ projects/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from customer_register.customer import Customer
+from django.contrib.auth.decorators import user_passes_test
+from project_management.decorators import customer_and_staff_check
 
+@user_passes_test(customer_and_staff_check, login_url='/accounts/login/')
 def projects(request):
     customer = Customer(request.user)
     """
