b52d1838c5cd8792d31d9d054766fc6b268515fe
Andre Mashraghi <andre.mashraghi@gmail.com>
2018-04-23 11:14:58 -0400

    improved search functionality

--- search_submit/views.py
+++ search_submit/views.py
@@ -12,7 +12,7 @@ class Search_Submit_View(View):
     template_name = 'search_submit/search_submit.html'
     def post(self, request):
         def normalize_query(query_string,
-            findterms=re.compile(r'"([^"]+)"|(\S+)|(\d+)').findall,
+            findterms=re.compile(r'"([^"]+)"|(\S+)').findall,
             normspace=re.compile(r'\s{2,}').sub):
 
             '''
@@ -22,16 +22,8 @@ class Search_Submit_View(View):
                 ['some', 'random', 'words', 'with quotes', 'and', 'spaces']
             '''
 
-            result = (normspace(' ',(t[0] or t[1]).strip()) for t in findterms(query_string))
-            #convert nums to decimals
-            def is_num(string):
-                try:
-                    float(string)
-                    return True
-                except ValueError as e:
-                    return False
+            return (normspace(' ',(t[0] or t[1]).strip()) for t in findterms(query_string))
 
-            return (t + '.00' for t in result if is_num(t))
 
         template = loader.get_template(self.template_name)
         query = request.POST.get('search')
@@ -44,7 +36,7 @@ class Search_Submit_View(View):
             Q(house__address__icontains=term) |
             Q(house__address__startswith=term) |
             Q(house__address__endswith=term) |
-            Q(start_amount=term)
+            Q(start_amount__startswith=term)
             for term in query_terms)
         )
         items = Job.objects.filter(queryset, house__customer=customer.customer)
