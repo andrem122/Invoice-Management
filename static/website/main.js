0bbff705b3f3556fd737b937aaa077755f652400
Andre Mashraghi <andre.mashraghi@gmail.com>
2019-03-24 10:28:02 -0400

    95% finished building landing home page

--- static/website/main.js
+++ static/website/main.js
@@ -2,25 +2,51 @@ document.addEventListener('DOMContentLoaded', function() {
 
   //animates from number A to number B
   function counter(element, start, end, duration) {
-    var range = end - start;
-    var current = start;
-    var increment = end > start ? 1 : -1;
-    var timer = setInterval(function() {
-        current += increment;
-        element.innerHTML = current;
-        if (current == end) {
-            clearInterval(timer);
-        }
-    }, duration);
+    if(!element.classList.contains('animated')) {
+      var range = end - start;
+      var current = start;
+      var increment = end > start ? 1 : -1;
+      var step_time = Math.abs(Math.floor(duration / range));
+
+      var timer = setInterval(function() {
+          current += increment;
+          element.innerHTML = current;
+          if (current == end) {
+              clearInterval(timer);
+          }
+      }, step_time);
+
+      element.classList.add('animated');
+    }
   }
 
-  var counters = document.getElementsByClassName('number');
+  //check if element in viewport
+  var counters_container = document.getElementsByClassName('counters')[0];
 
-  //loop through counters
-  var l = counters.length;
-  for(var i = 0; i < l; i++) {
-    var counter_obj = JSON.parse(counters[i].getAttribute('data-count-params'));
-    counter(counters[i], counter_obj.num_begin, counter_obj.num_end, counter.duration);
+  function in_viewport(element) {
+    var bounding = element.getBoundingClientRect();
+    var window_width = window.innerWidth || document.documentElement.clientWidth;
+    var window_height = window.innerHeight || document.documentElement.clientHeight;
+
+    if (bounding.top >= 0 && bounding.left >= 0 && bounding.right <= window_width && bounding.bottom <= window_height) {
+      return true;
+    } else {
+      return false;
+    }
+
   }
+
+  window.addEventListener('scroll', function() {
+    if(in_viewport(counters_container)) {
+      var counters = document.getElementsByClassName('number');
+
+      //loop through counters and animate them
+      var l = counters.length;
+      for(var i = 0; i < l; i++) {
+        var counter_obj = JSON.parse(counters[i].getAttribute('data-count-params'));
+        counter(counters[i], counter_obj.num_begin, counter_obj.num_end, counter_obj.duration);
+      }
+    }
+  });
 
 });
