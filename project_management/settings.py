4a9d599b97956bbbf93572de10aac31a7c46d466
Andre Mashraghi <andre.mashraghi@gmail.com>
2019-10-05 18:52:48 -0400

    c

--- project_management/settings.py
+++ project_management/settings.py
@@ -136,12 +136,10 @@ WSGI_APPLICATION = 'project_management.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'sqlite3.db',                      # Or path to database file if using sqlite3.
-    }
-}
+import dj_database_url
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
