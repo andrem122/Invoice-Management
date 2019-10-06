b949e8e92a545f243bbf6337a41db481297f4df5
Andre Mashraghi <andre.mashraghi@gmail.com>
2019-10-06 09:25:18 -0400

    added redis to go

--- project_management/settings.py
+++ project_management/settings.py
@@ -71,21 +71,21 @@ THIRD_PARTY_APPS = (
 
 INSTALLED_APPS = DJANGO_APPS + LOCAL_APPS + THIRD_PARTY_APPS
 
-# DRAMATIQ_BROKER = {
-#     "BROKER": "dramatiq.brokers.rabbitmq.RabbitmqBroker",
-#     "OPTIONS": {
-#         "url": "amqp://localhost:5672",
-#     },
-#     "MIDDLEWARE": [
-#         "dramatiq.middleware.Prometheus",
-#         "dramatiq.middleware.AgeLimit",
-#         "dramatiq.middleware.TimeLimit",
-#         "dramatiq.middleware.Callbacks",
-#         "dramatiq.middleware.Retries",
-#         "django_dramatiq.middleware.AdminMiddleware",
-#         "django_dramatiq.middleware.DbConnectionsMiddleware",
-#     ]
-# }
+DRAMATIQ_BROKER = {
+    "BROKER": "dramatiq.brokers.rabbitmq.RabbitmqBroker",
+    "OPTIONS": {
+        "url": "amqp://localhost:5672",
+    },
+    "MIDDLEWARE": [
+        "dramatiq.middleware.Prometheus",
+        "dramatiq.middleware.AgeLimit",
+        "dramatiq.middleware.TimeLimit",
+        "dramatiq.middleware.Callbacks",
+        "dramatiq.middleware.Retries",
+        "django_dramatiq.middleware.AdminMiddleware",
+        "django_dramatiq.middleware.DbConnectionsMiddleware",
+    ]
+}
 
 # Defines which database should be used to persist Task objects when the
 # AdminMiddleware is enabled.  The default value is "default".
