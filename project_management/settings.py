960bb93af80912aa285048991226faf42cfb7774
Andre Mashraghi <andre.mashraghi@gmail.com>
2019-10-07 09:04:10 -0400

    changed settings.py debug value

--- project_management/settings.py
+++ project_management/settings.py
@@ -21,6 +21,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
+DEBUG_PROPAGATE_EXCEPTIONS = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'project-management-novaone.herokuapp.com']
 
