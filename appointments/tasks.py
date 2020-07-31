90568505ec4da9cca5f0db9d8184fec714b956dd
Andre Mashraghi <andre.mashraghi@gmail.com>
2020-07-31 15:53:12 -0400

    restored removed files

--- appointments/tasks.py
+++ appointments/tasks.py
@@ -76,8 +76,10 @@ def send_new_appointment_notification(appointment_id, appointment_model):
 
     if customer_type == 'MW':
         # Additional appointment details based on customer type
+        print('MEDICAL WORKER')
         appointment_address = appointment.address
         appointment_city = appointment.city
+        print(appointment_city)
         appointment_zip = appointment.zip
         appointment_email = appointment.email
         appointment_date_of_birth = appointment.date_of_birth
@@ -112,6 +114,7 @@ def send_new_appointment_notification(appointment_id, appointment_model):
                 appointment_test_type=appointment_test_type,
                 appointment_status=appointment_status,
             )
+        print(text_message)
 
         context = {
                 'customer_type': customer_type,
