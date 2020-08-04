d5c09e9b6776f8bd19c12890186370eba1105fc6
Andre Mashraghi <andre.mashraghi@gmail.com>
2020-08-04 08:37:43 -0400

    fixed the commands in tasks/restart_rundramatiq.sh

--- tasks/restart_rundramatiq.sh
+++ tasks/restart_rundramatiq.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-curl -n -X DELETE https://api.heroku.com/apps/project-management-novaone/dynos \
+curl -n -X DELETE https://api.heroku.com/apps/project-management-novaone-two/dynos \
   -H "Content-Type: application/json" \
   -H "Accept: application/vnd.heroku+json; version=3" \
   -H "Authorization: Bearer $HEROKU_API_KEY"
 
-curl -n -X POST https://api.heroku.com/apps/project-management-novaone/dynos \
+curl -n -X POST https://api.heroku.com/apps/project-management-novaone-two/dynos \
   -d '{
   "attach": false,
   "command": "python manage.py rundramatiq",
